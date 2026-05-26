cba8d07a0c01d600e2785c65601bb24ae97df221
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-05-26 09:55:17 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,12 +6,19 @@ const FILES_TO_CACHE = [
   "./index.html",
   "./style.css",
   "./app.js",
-  "./manifest.json",
-  "./library/sample.epub"
-
+  "./manifest-beta.json",
+  "./library/sample.epub",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
+/* =========================
+   INSTALL
+========================= */
+
 self.addEventListener("install", event => {
+
+  self.skipWaiting();
 
   event.waitUntil(
 
@@ -27,6 +34,10 @@ self.addEventListener("install", event => {
   );
 
 });
+
+/* =========================
+   ACTIVATE
+========================= */
 
 self.addEventListener("activate", event => {
 
@@ -50,20 +61,62 @@ self.addEventListener("activate", event => {
         );
 
       })
+      .then(() => {
+
+        return self.clients.claim();
+
+      })
 
   );
 
 });
 
+/* =========================
+   FETCH
+========================= */
+
 self.addEventListener("fetch", event => {
+
+  if (
+    event.request.method !== "GET"
+  ) {
+
+    return;
+
+  }
 
   event.respondWith(
 
     caches.match(event.request)
-      .then(response => {
+      .then(cachedResponse => {
 
-        return response ||
-          fetch(event.request);
+        if (cachedResponse) {
+
+          return cachedResponse;
+
+        }
+
+        return fetch(event.request)
+          .then(networkResponse => {
+
+            return caches.open(CACHE_NAME)
+              .then(cache => {
+
+                cache.put(
+                  event.request,
+                  networkResponse.clone()
+                );
+
+                return networkResponse;
+
+              });
+
+          });
+
+      })
+      .catch(() => {
+
+        return caches.match("./index.html");
 
       })
 
