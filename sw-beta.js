feac9452bc9300cc1325fdc7de46c2f63f12917a
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-12 05:36:19 +0000

    Update app version to v2.1.4

--- sw-beta.js
+++ sw-beta.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v2.1.1";
+const APP_VERSION  = "v2.1.4";
 const CACHE_NAME   = `alatipha-ges-pasco-${APP_VERSION}`;
 
 const FILES_TO_CACHE = [
