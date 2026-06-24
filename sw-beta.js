cb0662ef7c0fbdf72cff6896ba102fc8c74d92de
Abdul-Latif Ahmed <alatipha@ymail.com>
2026-06-24 05:39:41 +0000

    Update and rename sw-beta-3.js to sw-beta.js

--- sw-beta.js
+++ sw-beta.js
@@ -7,7 +7,7 @@
    get a "New version available" prompt on next visit.
 ===================================================== */
 
-const APP_VERSION  = "v3.0.5";
+const APP_VERSION  = "v3.5.0";
 const CACHE_NAME   = `alatipha-ges-pasco-beta-${APP_VERSION}`;
 
 /* ====================
